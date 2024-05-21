08256777bdc8c1898bfb073c2b338af685adff01
Artem <artempolzikov25@gmail.com>
2024-05-21 15:09:40 +0300

    Правки

--- HomeWork 11/src/Main.java
+++ HomeWork 11/src/Main.java
@@ -2,7 +2,7 @@ import java.time.LocalDate;
 
 public class Main {
     //Task 1 method
-    public static void checkLongYear(long year) {
+    public static void checkLeapYear(long year) {
         boolean yearDivBy4 = year % 4 == 0;
         boolean yearDivBy100 = year % 100 != 0;
         boolean yearDivBy400 = year % 400 == 0;
@@ -17,34 +17,36 @@ public class Main {
 
 
     //Task 2 method
-    public static void checkSoftwareVersion(int number, int clientOS) {
+    public static void recommendAppVersionBasedOnYearAndOS(int number, int clientOS) {
         int currentYear = LocalDate.now().getYear();
         if (clientOS == 0 && number < currentYear && number > 0) {
             System.out.println("Установите облегчённую версию приложения для iOS по ссылке.");
-        } else if (clientOS == 0 && number >= 2015) {
+        } else if (clientOS == 0 && number >= currentYear) {
             System.out.println("Установите версию приложения для iOS по ссылке.");
         }
         if (clientOS == 1 && number < currentYear && number > 0) {
             System.out.println("Установите облегчённую версию приложения для Android по ссылке.");
         } else if (clientOS == 1 && number >= currentYear && number > 0) {
             System.out.println("Установите версию приложения для Android по ссылке.");
-        } else {
-            System.out.println("Вы указали данные некорректно");
         }
     }
     //===================================================================================
 
 
     //Task 3 method
-    public static void checkDeliveryTime(int deliveryDistance) {
+    public static int calculateDeliveryTime(int deliveryDistance) {
+        int deliveryTime;
         if (deliveryDistance <= 20 && deliveryDistance >= 0) {
-            System.out.println("Доставка будет осуществлена в течение одного дня.");
+            deliveryTime = 1;
+            return deliveryTime;
         } else if (deliveryDistance <= 60 && deliveryDistance >= 0) {
-            System.out.println("Доставка будет осуществлена в течение двух дней.");
+            deliveryTime = 2;
+            return deliveryTime;
         } else if (deliveryDistance <= 100 && deliveryDistance >= 0) {
-            System.out.println("Доставка будет осуществлена в течение трёх дней.");
+            deliveryTime = 3;
+            return deliveryTime;
         } else {
-            System.out.println("Вы слишком далеко или указали некорректные данные.");
+            return 0;
         }
     }
     //===================================================================================
@@ -52,18 +54,19 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Task 1");
-        checkLongYear(2015);
-        checkLongYear(1588);
-        checkLongYear(2013);
+        checkLeapYear(2015);
+        checkLeapYear(1588);
+        checkLeapYear(2013);
 
         System.out.println("\nTask 2");
-        checkSoftwareVersion(2015, 0);
-        checkSoftwareVersion(2015, 1);
-        checkSoftwareVersion(2015, 2);
+        recommendAppVersionBasedOnYearAndOS(2015, 0);
+        recommendAppVersionBasedOnYearAndOS(2024, 2);
+        recommendAppVersionBasedOnYearAndOS(2012, 1);
+
 
         System.out.println("\nTask 3");
-        checkDeliveryTime(95);
-        checkDeliveryTime(1);
-        checkDeliveryTime(194);
+        System.out.println(calculateDeliveryTime(95));
+        System.out.println(calculateDeliveryTime(1));
+        System.out.println(calculateDeliveryTime(194));
     }
 }
